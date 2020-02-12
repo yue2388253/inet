24833b32f4c052649f22db83fb2fc3ddfe542925
Zoltan Bojthe <zbojthe@omnetpp.org>
2020-02-12 18:28:13 +0100

    EtherPhy

--- src/inet/physicallayer/ethernet/EtherPhy.cc
+++ src/inet/physicallayer/ethernet/EtherPhy.cc
@@ -15,6 +15,7 @@
 // along with this program; if not, see <http://www.gnu.org/licenses/>.
 //
 
+#include "inet/common/ModuleAccess.h"
 #include "inet/common/ProtocolTag_m.h"
 #include "inet/common/packet/Packet.h"
 #include "inet/linklayer/ethernet/EtherPhyFrame_m.h"
@@ -26,31 +27,82 @@ namespace physicallayer {
 
 Define_Module(EtherPhy);
 
+simsignal_t EtherPhy::txStateChangedSignal = registerSignal("txStateChanged");
+simsignal_t EtherPhy::rxStateChangedSignal = registerSignal("rxStateChanged");
+
 void EtherPhy::initialize(int stage)
 {
-    cSimpleModule::initialize(stage);
+    cPhyModule::initialize(stage);
     if (stage == INITSTAGE_LOCAL) {
         physInGate = gate("phys$i");
+        physOutGate = gate("phys$o");
         upperLayerInGate = gate("upperLayerIn");
+        bitrate = par("bitrate");
+        duplexMode = par("duplexMode");
+        if (!duplexMode)
+            throw cRuntimeError("half-duplex currently not supported.");
+        displayStringTextFormat = par("displayStringTextFormat");
+        sendRawBytes = par("sendRawBytes");
+
+        // initialize connected flag
+        connected = physOutGate->getPathEndGate()->isConnected() && physInGate->getPathStartGate()->isConnected();
+        if (!connected)
+            EV_WARN << "PHY not connected to a network.\n";
+
+        // initialize states
+        txState = connected ? TX_IDLE_STATE : TX_OFF_STATE;
+        rxState = connected ? RX_IDLE_STATE : RX_OFF_STATE;
+
+        // initialize self messages
+        endTxMsg = new cMessage("EndTransmission", ENDTRANSMISSION);
+    }
+    else if (stage == INITSTAGE_NETWORK_INTERFACE_CONFIGURATION) {
+        interfaceEntry = getContainingNicModule(this);
+    }
+    else if (stage == INITSTAGE_LINK_LAYER) {
+        cDatarateChannel *outTrChannel = dynamic_cast<cDatarateChannel *>(physOutGate->findTransmissionChannel());
+        if (outTrChannel != nullptr)
+            bitrate = outTrChannel->getDatarate();
+    }
+}
+
+void EtherPhy::changeTxState(TxState newState)
+{
+    if (newState != txState) {
+        txState = newState;
+        emit(txStateChangedSignal, newState);
     }
 }
 
 void EtherPhy::handleMessage(cMessage *message)
 {
-    if (message->getArrivalGate() == upperLayerInGate) {
-        Packet *packet = check_and_cast<Packet *>(message);
+    if (message->isSelfMessage()) {
+        if (message == endTxMsg) {
+            changeTxState(TX_IDLE_STATE);
+        }
+        else
+            throw cRuntimeError("Unknown self message received!");
+    }
+    else if (message->getArrivalGate() == upperLayerInGate) {
+        auto packet = check_and_cast<Packet *>(message);
         auto phyHeader = makeShared<EthernetPhyHeader>();
         packet->insertAtFront(phyHeader);
-        packet->addTagIfAbsent<PacketProtocolTag>()->setProtocol(&Protocol::ethernetPhy);
+        packet->clearTags();
+        packet->addTag<PacketProtocolTag>()->setProtocol(&Protocol::ethernetPhy);
         auto signal = new EthernetSignal(packet->getName());
-        signal->setSrcMacFullDuplex(true);
+        signal->setSrcMacFullDuplex(duplexMode);
+        signal->setBitrate(bitrate);
         signal->encapsulate(packet);
-        send(signal, "phys$o");
+        send(signal, physOutGate);
+        scheduleAt(transmissionChannel->getTransmissionFinishTime(), endTxMsg);
+        changeTxState(TX_TRANSMITTING_STATE);
     }
     else if (message->getArrivalGate() == physInGate) {
         auto signal = check_and_cast<EthernetSignalBase *>(message);
-        if (!signal->getSrcMacFullDuplex())
+        if (signal->getSrcMacFullDuplex() != duplexMode)
             throw cRuntimeError("Ethernet misconfiguration: MACs on the same link must be all in full duplex mode, or all in half-duplex mode");
+        if (signal->getBitrate() != bitrate)
+            throw cRuntimeError("Ethernet misconfiguration: MACs on the same link must be same bitrate");
         auto packet = check_and_cast<Packet *>(signal->decapsulate());
         delete signal;
         auto phyHeader = packet->popAtFront<EthernetPhyHeader>();
--- src/inet/physicallayer/ethernet/EtherPhy.h
+++ src/inet/physicallayer/ethernet/EtherPhy.h
@@ -19,25 +19,58 @@
 #define __INET_ETHERPHY_H
 
 #include "inet/common/INETDefs.h"
+#include "inet/networklayer/common/InterfaceEntry.h"
 
 namespace inet {
-
 namespace physicallayer {
 
-class INET_API EtherPhy : public cSimpleModule
+class INET_API EtherPhy : public cPhyModule
 {
+  public:
+    enum TxState {
+        TX_OFF_STATE = 0,
+        TX_IDLE_STATE,
+        TX_TRANSMITTING_STATE,
+    };
+
+    enum RxState {
+        RX_OFF_STATE = 0,
+        RX_IDLE_STATE,
+        RX_RECEIVING_STATE,
+    };
+
   protected:
-    cGate *physInGate = nullptr;
+    // Self-message kind values
+    enum SelfMsgKindValues {
+        ENDTRANSMISSION = 101,
+    };
+
+    const char *displayStringTextFormat = nullptr;
+    InterfaceEntry *interfaceEntry = nullptr;   // NIC module
+    cChannel *transmissionChannel = nullptr;    // transmission channel
+    cGate *physInGate = nullptr;    // pointer to the "phys$i" gate
+    cGate *physOutGate = nullptr;    // pointer to the "phys$o" gate
     cGate *upperLayerInGate = nullptr;
+    cMessage *endTxMsg = nullptr;
+    double bitrate = NaN;
+    bool   sendRawBytes = false;
+    bool   duplexMode = true;
+    bool   connected = false;    // true if connected to a network, set automatically by exploring the network configuration
+    TxState txState = TX_OFF_STATE;    // "transmit state" of the MAC
+    RxState rxState = RX_OFF_STATE;    // "receive state" of the MAC
+
+    static simsignal_t txStateChangedSignal;
+    static simsignal_t rxStateChangedSignal;
 
   public:
     virtual int numInitStages() const override { return NUM_INIT_STAGES; }
     virtual void initialize(int stage) override;
     virtual void handleMessage(cMessage *msg) override;
+
+    void changeTxState(TxState newState);
 };
 
 } // namespace physicallayer
-
 } // namespace inet
 
 #endif // ifndef __INET_ETHERPHY_H
