b8eed3616879f2498ada3c7150c0485c7014d344
Zoltan Bojthe <zbojthe@omnetpp.org>
2020-02-14 15:45:00 +0100

    NoEthFingerprintCalculator

---
+++ src/inet/common/NoEthFingerprintCalculator.cc
@@ -0,0 +1,38 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#include "inet/common/ModuleAccess.h"
+#include "inet/common/NoEthFingerprintCalculator.h"
+#include "inet/common/packet/Packet.h"
+#include "inet/linklayer/ethernet/EtherMac.h"
+#include "inet/linklayer/ethernet/EtherMacFullDuplex.h"
+
+namespace inet {
+
+Register_Class(NoEthFingerprintCalculator);
+
+void NoEthFingerprintCalculator::addEvent(cEvent *event)
+{
+    if (event->isMessage()) {
+        auto message = static_cast<cMessage *>(event);
+        auto arrivalModule = message->getArrivalModule();
+        if (dynamic_cast<EtherMac*>(arrivalModule) || dynamic_cast<EtherMacFullDuplex*>(arrivalModule))
+            return;
+    }
+    cSingleFingerprintCalculator::addEvent(event);
+}
+
+} // namespace
+
